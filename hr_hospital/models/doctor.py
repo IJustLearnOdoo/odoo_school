336bb9380d111d777b4a2163535bf0bff7824f9a
Oleksandr <oleksandr.panchenko@kyivstar.net>
2024-09-16 10:15:24 +0300

    Refactor specialization field formatting in Doctor model
    Reformatted the selection field `specialization` for readability by adding line breaks between each choice. This improves the code's maintainability and enhances its visual clarity.

--- hr_hospital/models/doctor.py
+++ hr_hospital/models/doctor.py
@@ -1,11 +1,17 @@
 from odoo import models, fields
 
+
 class Doctor(models.Model):
     _name = 'hr_hospital.doctor'
     _description = 'Doctor'
 
     name = fields.Char(required=True)
     specialization = fields.Selection(
-        [('neurologist', 'Neurologist'), ('psychiatrist', 'Psychiatrist'), ('therapist', 'Therapist')])
+        [
+            ('neurologist', 'Neurologist'),
+            ('psychiatrist', 'Psychiatrist'),
+            ('therapist', 'Therapist')
+        ]
+    )
     phone = fields.Char()
     patient_ids = fields.One2many('hr_hospital.patient', 'doctor_id')
