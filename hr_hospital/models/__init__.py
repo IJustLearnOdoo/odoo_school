524bcec5794f393f74dd5e87fd0c3d5a157734a0
Oleksandr <oleksandr.panchenko@kyivstar.net>
2024-09-23 12:36:31 +0300

    Add new wizard modules and data entries for hr_hospital
    Integrated bulk doctor assignment and disease report wizards. Updated manifest file to include new views, data files, and cron jobs. Added models for hr_hospital_person and diagnosis.

--- hr_hospital/models/__init__.py
+++ hr_hospital/models/__init__.py
@@ -1,4 +1,6 @@
+from . import hr_hospital_person
 from . import hr_hospital_disease
 from . import hr_hospital_doctor
 from . import hr_hospital_patient
 from . import hr_hospital_visit
+from . import diagnosis
